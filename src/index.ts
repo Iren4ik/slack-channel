2a1457a995163feb00b5581d82e17e15f58651c7
Ирена Коршакова <k.iren98@yandex.ru>
2024-07-06 19:00:03 +0300

    feat: add sidebar

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,32 @@
 import './styles.scss';
-import hash from './assets/icons/hash.svg';
-import lock from './assets/icons/lock.svg';
+import { channels } from "./utils/channels";
+import hashIcon from './assets/icons/hash.svg';
+import lockIcon from './assets/icons/lock.svg';
 
-console.log('Project started');
-console.log('Hash SVG:', hash);
-console.log('Lock SVG:', lock);
+const channelsList = document.getElementById("channels-list");
+const channelsHeader = document.getElementById("channels-header");
+const rollupIcon = document.querySelector(".rollup-icon");
+
+
+// Рендеринг списка каналов
+if (channelsList) {
+  channelsList.innerHTML = channels
+    .map((channel) => {
+      const iconSrc = channel.type === "public" ? hashIcon : lockIcon;
+      return `
+        <button>
+          <img src="${iconSrc}" alt="${channel.type} icon"/>
+          <p>${channel.name}</p>
+        </button>
+      `;
+    })
+    .join("");
+}
+
+// Добавляем функциональность раскрывающегося списка
+if (channelsHeader && channelsList && rollupIcon) {
+  channelsHeader.addEventListener('click', () => {
+    channelsList.classList.toggle('hidden');
+    rollupIcon.classList.toggle('rotated');
+  });
+}
--- src/utils/channels.ts
+++ src/utils/channels.ts
@@ -1,1 +1,26 @@
-export const channels = ['general', 'random', 'help'];
+export interface Channel {
+  name: string;
+  type: "public" | "private";
+}
+
+export const channels: Channel[] = [
+  { name: "slack-implementation", type: "public" },
+  { name: "change-approval", type: "public" },
+  { name: "culture-club", type: "public" },
+  { name: "customersuccess", type: "public" },
+  { name: "general", type: "public" },
+  { name: "slack-workflows", type: "public" },
+  { name: "marketintel", type: "public" },
+  { name: "mdc-updates", type: "public" },
+  { name: "product-feedback", type: "public" },
+  { name: "product-feedback", type: "public" },
+  { name: "product-feedback", type: "public" },
+  { name: "product-qa", type: "public" },
+  { name: "product-support", type: "public" },
+  { name: "product-team", type: "private" },
+  { name: "product-feedback", type: "public" },
+  { name: "product-feedback", type: "public" },
+  { name: "product-feedback", type: "public" },
+  { name: "product-qa", type: "public" },
+  { name: "product-support", type: "public" },
+];
