2516392c3b86fa0e9159df151bf87b952bcbaf45
Ирена Коршакова <k.iren98@yandex.ru>
2024-07-06 14:50:35 +0300

    feat: Create and configure a repository

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import './styles.scss';
+import hash from './assets/icons/hash.svg';
+import lock from './assets/icons/lock.svg';
+
+console.log('Project started');
+console.log('Hash SVG:', hash);
+console.log('Lock SVG:', lock);
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { HotModuleReplacementPlugin } = require('webpack');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    hot: true,
+    watchFiles: ['src/**/*', 'index.html']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader',
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new HotModuleReplacementPlugin(),
+  ],
+};
